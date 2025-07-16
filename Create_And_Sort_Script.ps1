aa1b178b059099ce4305354af4e0b992a27ac6a1
Bshling <roeishlingbaum@gmail.com>
2025-07-16 17:02:43 +0300

    Add files via upload

---
+++ Create_And_Sort_Script.ps1
@@ -0,0 +1,44 @@
+﻿
+#Variables
+$dest = 'C:\Destination\'
+$sour = 'C:\SourceEnd\'
+
+# Ensure destination folders exist
+$exist = Test-Path $dest
+
+if( $exist -eq $false)
+{
+ New-Item -Path ($dest + 'Docs') -ItemType Directory 
+
+ New-Item -Path ($dest + 'Images') -ItemType Directory 
+
+}
+#creating the array 
+$filestosort =Get-ChildItem -Path $sour
+
+#copying and filtering the files
+foreach($item in $filestosort)
+{
+#filtering the images
+if($item.Extension -eq '.bmp')
+{
+ 
+  $destdoc = $dest + 'Images'
+Copy-Item -path $item.FullName -Destination $destdoc
+
+
+}
+#Filtering the documents
+elseif($item.Extension -eq '.txt' -or $item.Extension -eq '.docx')
+
+{
+
+
+ $destdoc = $dest + 'Docs'
+Copy-Item -path $item.FullName -Destination $destdoc
+
+
+}
+}
+
+
